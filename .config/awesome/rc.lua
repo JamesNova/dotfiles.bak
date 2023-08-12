9b5c42b4c9c67c067a4c402a7c7d97a4896a3362
JamesNova <jamesnova911@gmail.com>
2023-08-12 11:22:16 -0300

    moving music

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -45,7 +45,7 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init("~/.config/awesome/themes/dracula/theme.lua")
+beautiful.init("~/.config/awesome/themes/nord/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
 terminal = "alacritty"
